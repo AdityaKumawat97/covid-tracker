423e7bc8636e61fc8159a468fc708da062f90c0b
Aditya_kumawat <31047132+AdityaKumawat97@users.noreply.github.com>
2020-05-01 17:44:00 +0530

    Initializing React- app
    Downloading dependencies, creating a react app

---
+++ src/components/Chart/Chart.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Chart = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default Chart;
---
+++ src/components/CountryPicker/CountryPicker.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CountryPicker = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default CountryPicker;
