d69135d5890ba7412494739cd534b211ea0d9b09
Aditya_kumawat <31047132+AdityaKumawat97@users.noreply.github.com>
2020-05-07 11:38:27 +0530

    implementing bar charts for inidvidual countries is complete, fixing minor errors

--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { fetchdailyData } from '../../api'
 import { Line, Bar } from 'react-chartjs-2'
 import styles from './Chart.module.css'
-const Chart = ({ data, country }) => {
+const Chart = ({ data: { confirmed, recovered, deaths }, country }) => {
     const [dailyData, setdailyData] = useState([])
 
     useEffect(() => {
@@ -12,7 +12,7 @@ const Chart = ({ data, country }) => {
         // console.log(dailyData)
         fetchAPI()
 
-    })
+    }, [])
 
     const lineChart = (
         dailyData.length ?
@@ -36,13 +36,14 @@ const Chart = ({ data, country }) => {
     )
 
     const barChart = (
-        data.confirmed ? (
+        confirmed ? (
             <Bar
                 data={{
                     labels: ['Infected', 'Recovered', 'Deaths'],
-                    datasets:[{
-                        label:'People',
-                        backgroundColor:[`rgba(0,0,255,0.5)`,`rgba(0,255,0,0.5)`,`rgba(255,0,0,0.5)`]
+                    datasets: [{
+                        label: 'People',
+                        backgroundColor: ['rgba(0,0,255,0.5)', 'rgba(0,255,0,0.5)', 'rgba(255,0,0,0.5)'],
+                        data: [confirmed.value, recovered.value, deaths.value]
                     }]
                 }}
                 options={{
@@ -54,7 +55,7 @@ const Chart = ({ data, country }) => {
 
     return (
         <div className={styles.container}>
-            {lineChart}
+            {country ? barChart : lineChart}
         </div>
     );
 }
--- src/components/CountryPicker/CountryPicker.jsx
+++ src/components/CountryPicker/CountryPicker.jsx
@@ -15,7 +15,7 @@ const CountryPicker = ({ handleCountryChange }) => {
 
         <FormControl className={styles.formControl}>
             <NativeSelect defaultValue='' onChange={(e)=>{handleCountryChange(e.target.value)}}>
-                <option value="global" >Global</option>
+                <option value="" >Global</option>
                 {fetchedCountries.map((country, idx) => <option key={idx} value={country}>{country}</option>)}
             </NativeSelect>
         </FormControl>
